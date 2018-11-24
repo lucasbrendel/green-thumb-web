80530a4a44516401db7b4dcc4ada5b4f5b45d579
Lucas Brendel <lucasbrendel@gmail.com>
2018-11-24 00:18:53 -0600

    disabling all need for sql3

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@
 extern crate rocket;
 extern crate chrono;
 extern crate rocket_contrib;
-extern crate rusqlite;
+// extern crate rusqlite;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_rusqlite;
+// extern crate serde_rusqlite;
 extern crate strum;
 #[macro_use]
 extern crate strum_macros;
@@ -17,7 +17,7 @@ extern crate log;
 extern crate simplelog;
 
 #[allow(dead_code)]
-mod data;
+// mod data;
 mod logging;
 mod routes;
 
@@ -25,7 +25,7 @@ use rocket_contrib::templates::Template;
 
 fn main() {
     logging::logging_init();
-    let _mgr = data::DataMgr::new(String::from("./db/green-thumb.db"));
+    // let _mgr = data::DataMgr::new(String::from("./db/green-thumb.db"));
     rocket::ignite()
         .mount(
             "/",
