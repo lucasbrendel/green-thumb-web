6004c222167ab896375dcb319abff927892e9d1f
Lucas Brendel <lucasbrendel@gmail.com>
2018-11-23 22:16:35 -0600

    adding missing routes. thing still 500's

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -5,7 +5,7 @@ use maud::{html, Markup};
 use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 
-/// This is the entrypoint for our yew client side app.
+/// This is the entrypoint
 #[get("/")]
 fn index(db: State<Arc<sled::Tree>>) -> Markup {
     // maud macro
@@ -24,13 +24,13 @@ pub fn favicon() -> Option<NamedFile> {
 
 /// Serve static assets from the "static" folder.
 #[get("/static/<path..>")]
-fn static_file(path: PathBuf) -> Option<NamedFile> {
+pub fn static_file(path: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(path)).ok()
 }
 
 // TODO: remove this when we figure out how to change the native Rust
 // WebAssembly's generated JavaScript code to point at "static/" prefix.
-#[get("/ui.wasm")]
-fn ugly_hack() -> Option<NamedFile> {
+#[get("/static/green-thumb.wasm")]
+pub fn ugly_hack() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/ui.wasm")).ok()
 }
