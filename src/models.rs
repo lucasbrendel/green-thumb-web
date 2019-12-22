d6395ef6ad96497cbe1b8e40079b5ca15df202f1
Lucas Brendel <lucasbrendel@gmail.com>
2019-12-22 15:04:56 -0600

    removing old commented out stuff

--- src/models.rs
+++ src/models.rs
@@ -6,12 +6,6 @@ pub struct Plant {
     pub id: i32,
     /// Name of a plant.
     pub title: String,
-    /// Seasonal type of plant
-    // pub plant_type: PlantType,
-    /// Growing zones defined by USDA that a plant can survive in. This is limited to first 10 zones.
-    // pub zones: Vec<u8>,
-    /// Any description or textual things to track about the plant.
-    // pub notes: String,
     /// Number of days from planting until germination occurs
     pub days_to_maturity: i32,
 }
@@ -20,8 +14,5 @@ pub struct Plant {
 #[table_name = "plants"]
 pub struct NewPlant<'a> {
     pub title: &'a str,
-    // pub plant_type: &'a PlantType,
-    // pub zones: Vec<u8>,
-    // pub notes: &'a str,
     pub days_to_maturity: i32,
 }
